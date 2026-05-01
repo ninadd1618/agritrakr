51e4e33a497995133eaae534b16d24364f313682
PatilNinad18 <iampatil1804@gmail.com>
2026-05-01 15:48:14 +0530

    Added some server changes

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -4,11 +4,14 @@ import connectDB from './config/db.js';
 import { appLogger } from './config/logger.js';
 import { env } from './config/env.js';
 
-dotenv.config({
-  path: './.env'
-});
+dotenv.config();
 
-const PORT = env.PORT;
+const PORT = process.env.PORT || env.PORT || 4000;
+
+console.log("ENV CHECK:", {
+  PORT,
+  MONGO: process.env.MONGODB_URI ? "SET" : "NOT SET"
+});
 
 connectDB()
   .then(() => {
@@ -18,4 +21,5 @@ connectDB()
   })
   .catch((err) => {
     appLogger.error("MONGO db connection FAILED !!!", err);
+    process.exit(1);
   });
